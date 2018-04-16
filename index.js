a487de3bafd2da4060ca79268398b7913c2dac68
jaredianmills <jaredianmills@gmail.com>
2018-04-16 14:50:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function add(a, b) {
-  return a + b
+  return a + b;
 }
 
 function subtract(a, b) {
-  return a - b
+  return a - b;
 }
 
 function multiply(a, b) {
-  return a * b
+  return a * b;
 }
 
 function divide(a, b) {
-  return a / b
+  return a / b;
 }
 
 function inc(n) {
-  n += 1
+  n += 1;
 }
 
 function dec(n) {
-  n -= 1
+  n -= 1;
 }
